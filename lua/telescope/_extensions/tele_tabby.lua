64e23d6abaab4503a92121f908b604b339d88f61
Ted Kulp <ted.kulp@wizehive.com>
2022-08-23 08:01:27 -0400

    feat: multiple changes
    - Only show the root directory name for each tab
    - Only show one entry for each tab
    - Filter out neo-tree windows and empty tabs
    - Use grep previewer
    - Formatting

--- lua/telescope/_extensions/tele_tabby.lua
+++ lua/telescope/_extensions/tele_tabby.lua
@@ -25,13 +25,12 @@ local scan = require('plenary.scandir')
 
 -- This is our action when a window is chosen
 local function goto_window(prompt_bufnr)
-    actions.close(prompt_bufnr)
-    local entry = action_state.get_selected_entry()
-    -- entry.value gives us a window id
-    -- this call switches to the tab containinfg the window
-    vim.api.nvim_set_current_win( entry.value )
+  actions.close(prompt_bufnr)
+  local entry = action_state.get_selected_entry()
+  -- entry.value gives us a window id
+  -- this call switches to the tab containinfg the window
+  vim.api.nvim_set_current_win(entry.value)
 end
-
 
 --[[
 -- thanks to conni for the fucntion
@@ -45,7 +44,7 @@ local root_pattern
 root_pattern = function(start, pattern)
   if start == '/' then return nil end
   local res = scan.scan_dir(start, { search_pattern = pattern, hidden = true, add_dirs = true, depth = 1 })
-  if table.getn(res) == 0 then
+  if #res == 0 then
     local new = start .. '/../'
     return root_pattern(vim.loop.fs_realpath(new), pattern)
   else
@@ -81,18 +80,18 @@ local function make_entry(opts)
     end
     -- ]]
 
-      local displayer = entry_display.create {
-        separator = "",
-        items = {
-          --{ width = opts.bufnr_width },
-          { width = 2 },
-          { width = 5 },
-          { width = (icon_width + 3) },
-          { width = string.len( entry.path_start ) },
-          { width = 1 },
-          { remaining = true },
-        },
-      }
+    local displayer = entry_display.create {
+      separator = "",
+      items = {
+        --{ width = opts.bufnr_width },
+        { width = 2 },
+        { width = 5 },
+        { width = (icon_width + 3) },
+        { width = string.len(entry.path_start) },
+        --[[ { width = 1 }, ]]
+        --[[ { remaining = true }, ]]
+      },
+    }
 
     local icon, hl_group = utils.get_devicons(entry.filename, disable_devicons)
 
@@ -103,13 +102,13 @@ local function make_entry(opts)
 
     local path_end_highlight = state.use_highlighter and hl_group or nil
     return displayer {
-      { entry.tabidx, "TelescopeResultsNumber"},
-      { entry.indicator, "TelescopeResultsComment"},
+      { entry.tabidx, "TelescopeResultsNumber" },
+      { entry.indicator, "TelescopeResultsComment" },
       { icon, hl_group },
       { entry.path_start },
       { '/', "TelescopeResultsNumber" },
       { entry.path_end, path_end_highlight },
-      }
+    }
   end
 
   return function(entry)
@@ -144,98 +143,108 @@ local function make_entry(opts)
   end
 end
 
-
 --[[
 -- this is based on internal.buffers
 -- ]]
 
 local function list(opts)
-    opts = opts or {}
+  opts = opts or {}
 
-    -- use this code to remove windows which can't be focussed
-    -- local win_conf = vim.api.nvim_win_get_config(winnr)
-    --  local is_focusable = win_conf.relative == '' or win_conf.focusable
+  -- use this code to remove windows which can't be focussed
+  -- local win_conf = vim.api.nvim_win_get_config(winnr)
+  --  local is_focusable = win_conf.relative == '' or win_conf.focusable
 
-    -- Get a list of tabs and then find all the windows in each tab
-    local tabnrs = vim.api.nvim_list_tabpages()
-    local current_win = vim.api.nvim_get_current_win()
-    local windows = {}
+  -- Get a list of tabs and then find all the windows in each tab
+  local tabnrs = vim.api.nvim_list_tabpages()
+  local current_win = vim.api.nvim_get_current_win()
+  local windows = {}
 
-    -- set the selection to the default 1 but we'll try to set it to the current tab
-    -- use idx to count up through the windows we find and use it to set selection_idx
-    local selection_idx = 1
-    local idx = 0
+  -- set the selection to the default 1 but we'll try to set it to the current tab
+  -- use idx to count up through the windows we find and use it to set selection_idx
+  local selection_idx = 1
+  local idx = 0
 
-    -- get both tabidx and tabnr, a tab always keeps its tabnr
-    -- but it can be moved to a different tabidx with tabmove
-    -- or when another tab gets deleted
-    for tabidx, tabnr in ipairs(tabnrs) do
-        -- Get the windows of this tab
-        local windownrs = vim.api.nvim_tabpage_list_wins(tabnr)
-        for windownr, windowid in ipairs(windownrs) do
-            idx = idx+1
-            if windowid == current_win then
-                -- found the current window
-                selection_idx = idx
-            end
+  -- get both tabidx and tabnr, a tab always keeps its tabnr
+  -- but it can be moved to a different tabidx with tabmove
+  -- or when another tab gets deleted
+  for tabidx, tabnr in ipairs(tabnrs) do
+    -- Get the windows of this tab
+    local windownrs = vim.api.nvim_tabpage_list_wins(tabnr)
+    for windownr, windowid in ipairs(windownrs) do
+      idx = idx + 1
+      if windowid == current_win then
+        -- found the current window
+        selection_idx = idx
+      end
 
-            -- the bufnr
-            local bufnr = vim.api.nvim_win_get_buf(windowid)
+      -- the bufnr
+      local bufnr = vim.api.nvim_win_get_buf(windowid)
 
-            -- find the cwd of this window, check in order of priority, lcd, tcd, cwd
-            local cwd = vim.fn.expand(
-                vim.fn.haslocaldir( windownr, tabidx ) and vim.fn.getcwd( windownr, tabidx ) or
-                vim.fn.haslocaldir( -1, tabidx ) and vim.fn.getcwd( -1, tabidx) or
-                vim.fn.getcwd()
-            )
+      -- find the cwd of this window, check in order of priority, lcd, tcd, cwd
+      local cwd = vim.fn.expand(
+        vim.fn.haslocaldir(windownr, tabidx) and vim.fn.getcwd(windownr, tabidx) or
+        vim.fn.haslocaldir(-1, tabidx) and vim.fn.getcwd(-1, tabidx) or
+        vim.fn.getcwd()
+      )
 
-            -- find the "project root" from the cwd, for now look for .git
-            -- TODO - allow a user to configure how we find the project root
-            --local git_root = vim.fn.systemlist("git -C " .. cwd .. " rev-parse --show-toplevel")[1]
-            --local git_root = require('lspconfig.util').root_pattern(".git")(cwd)
-            local git_root =  root_pattern(cwd, '%.git$')
+      -- find the "project root" from the cwd, for now look for .git
+      -- TODO - allow a user to configure how we find the project root
+      --local git_root = vim.fn.systemlist("git -C " .. cwd .. " rev-parse --show-toplevel")[1]
+      --local git_root = require('lspconfig.util').root_pattern(".git")(cwd)
+      local git_root = root_pattern(cwd, '%.git$')
 
-            local project_root = vim.fn.expand( opts.project_root or git_root or cwd)
-            -- include the parent directory of the .git file
-            project_root = project_root:gsub('[^/]+/?$', '')
-
-
-            -- split the path
-            -- path_start - from the project_root to the cwd, including the name
-            -- path_end - from the cwd to the file
-
-            --local path_start = path.normalize( cwd, project_root:gsub ('[^/]+/?$', '') )
-            local path_start = Path:new(cwd):normalize(project_root)
-            local info = vim.fn.getbufinfo(bufnr)[1]
-            local path_end = Path:new(info.name):normalize(cwd) or ''
+      local project_root = vim.fn.expand(opts.project_root or git_root or cwd)
+      -- include the parent directory of the .git file
+      project_root = project_root:gsub('[^/]+/?$', '')
 
 
-            local element = {
-                path_start = path_start,
-                path_end = path_end,
-                tabnr = tabnr,
-                tabidx = tabidx,
-                windownr = windownr,
-                windowid = windowid,
-                info = info
-            }
-            table.insert(windows, element)
+      -- split the path
+      -- path_start - from the project_root to the cwd, including the name
+      -- path_end - from the cwd to the file
+
+      --local path_start = path.normalize( cwd, project_root:gsub ('[^/]+/?$', '') )
+      local path_start = Path:new(cwd):normalize(project_root)
+      local info = vim.fn.getbufinfo(bufnr)[1]
+      local path_end = Path:new(info.name):normalize(cwd) or ''
+
+
+      local element = {
+        path_start = path_start,
+        path_end = path_end,
+        tabnr = tabnr,
+        tabidx = tabidx,
+        windownr = windownr,
+        windowid = windowid,
+        info = info
+      }
+
+      if #element.path_start > 0 and #element.path_end > 0 then
+        -- filter out neo-tree.  Need to either add nvimtree or make this
+        -- configurable
+        if string.find(path_end, "neo-tree filesystem", 0, true) == nil then
+          table.insert(windows, element)
+
+          -- we only need one window per tab
+          break
         end
+      end
     end
+  end
 
   pickers.new(opts, {
-    prompt_title = 'Tabs',
-    finder    = finders.new_table {
+    prompt_title            = 'Tabgroup List',
+    finder                  = finders.new_table {
       results = windows,
       entry_maker = opts.entry_maker or make_entry(opts)
     },
-    sorter = sorters.get_fzy_sorter(opts),
-      attach_mappings = function(_, map)
-        -- use our custom action to go the window id
-        map( 'i', '<CR>', goto_window)
-        map( 'n', '<CR>', goto_window)
-        return true
-        end,
+    previewer               = require("telescope.config").values.grep_previewer(opts),
+    sorter                  = sorters.get_fzy_sorter(opts),
+    attach_mappings         = function(_, map)
+      -- use our custom action to go the window id
+      map('i', '<CR>', goto_window)
+      map('n', '<CR>', goto_window)
+      return true
+    end,
     default_selection_index = selection_idx,
   }):find()
 end
@@ -246,8 +255,8 @@ end
 
 return telescope.register_extension {
   setup = function(ext_config)
-  set_config_state('project_root', ext_config.project_root, '.git')
-  set_config_state('use_highlighter', ext_config.use_highlighter, true)
+    set_config_state('project_root', ext_config.project_root, '.git')
+    set_config_state('use_highlighter', ext_config.use_highlighter, true)
   end,
   exports = {
     list = list,
